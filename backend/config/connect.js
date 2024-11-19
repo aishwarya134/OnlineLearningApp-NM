099f5ca10225afa90328c10d8a7cad893c560751
Aishwarya <112769433+aishwarya134@users.noreply.github.com>
2024-11-19 10:46:27 +0530

    Add files via upload

---
+++ backend/config/connect.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const connectionOfDb = () => {
+  mongoose
+    .connect(process.env.MONGO_DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      console.log(err);
+      
+    });
+};
+
+module.exports = connectionOfDb;
